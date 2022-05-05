5d147fd7a4cfe41de8269ddfe41f291d265cc9f1
Dasun <dasun.gunasinghe@qut.edu.au>
2022-05-05 16:57:13 +1000

    Updated to Handle CB2 and CB3 variants of UR
    - Tested with UR10 (CB2) Running Polyscope V1.8.x
    - Added updates to robot bringup based on hw_version (cb2 or cb3). cb3 is default
    - Added new real yaml configs for handling cb2 and cb3
    - Updated URROSRobot with cb3 sepecifc services (not required for cb2)

--- armer_ur/robots/URROSRobot.py
+++ armer_ur/robots/URROSRobot.py
@@ -26,14 +26,23 @@ from ur_dashboard_msgs.msg import RobotMode, SafetyMode
 class URROSRobot(ROSRobot):
     def __init__(self,
                  robot: rtb.robot.Robot,
+                 hw_version: str = None,
                  controller_name: str = None,
                  recover_on_estop: bool = True,
                  *args,
                  **kwargs):
         super().__init__(robot, *args, **kwargs)
         
+        # ----- Debugging Output ----------------
+        rospy.loginfo(f"Armer UR -> Configured hw_version: {hw_version}")
+
+        self.hw_version = hw_version \
+            if hw_version else 'cb3'
+
         self.controller_name = controller_name \
             if controller_name else self.joint_velocity_topic.split('/')[1]
+        
+        rospy.loginfo(f"controller name: {self.controller_name}")
 
         self.recover_on_estop = recover_on_estop
         self.last_estop_state = 0
@@ -55,13 +64,15 @@ class URROSRobot(ROSRobot):
         self.safety_state = None
 
         # Error recovery services
-        self.unlock_proxy = rospy.ServiceProxy('/ur_hardware_interface/dashboard/brake_release', Trigger)
-        self.unlock_proxy.wait_for_service()
+        # NOTE: this section should not be run when using CB2 models
+        if self.hw_version == 'cb3':
+            self.unlock_proxy = rospy.ServiceProxy('/ur_hardware_interface/dashboard/brake_release', Trigger)
+            self.unlock_proxy.wait_for_service()
 
-        self.reset_proxy = rospy.ServiceProxy('/ur_hardware_interface/dashboard/play', Trigger)
-        self.reset_proxy.wait_for_service()
+            self.reset_proxy = rospy.ServiceProxy('/ur_hardware_interface/dashboard/play', Trigger)
+            self.reset_proxy.wait_for_service()
 
-        self.recover_cb(EmptyRequest())
+            self.recover_cb(EmptyRequest())
         
 
     def recover_cb(self, req: EmptyRequest) -> EmptyResponse: # pylint: disable=no-self-use
